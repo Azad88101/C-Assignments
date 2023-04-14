2a18ba06d9b149c191393f408a5e9a7272e5d463
Ansalpandey110043 <pandeyansal77@gmail.com>
2023-04-14 19:08:17 +0530

    Arithmatic Operators

---
+++ Arithmatic_Operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a  = 4;
+    int b = 2;
+
+    int sum = a + b;
+    int subtraction = a - b;
+    int product = a * b;
+    int devide = a / b;
+    int remainder = a % b;
+
+    printf("Sum is:- %d \n", sum);
+    printf("Subtraction is:- %d \n", subtraction);
+    printf("Product is:- %d \n", product);
+    printf("Devision is:- %d \n", devide);
+    printf("Remainder is:- %d \n", remainder);
+    return 0;
+}
