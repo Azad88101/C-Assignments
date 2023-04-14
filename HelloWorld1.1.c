698c6c656bfb817bdcd0d759d77b7dee69a7f169
Ansalpandey110043 <pandeyansal77@gmail.com>
2023-04-14 18:23:31 +0530

    College assignments

---
+++ HelloWorld1.1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>    // Preprocessor Directives
+int main()        // main() Function section / Entry Point
+{
+    printf("Hello, World!");   //Body of Main function
+    return 0;      // Return value
+}
