a667d7d67f9788d29b49cc6fd45e1f92636b4d8d
Ansalpandey110043 <pandeyansal77@gmail.com>
2023-04-14 20:17:20 +0530

    Marksheet.c added

---
+++ Marksheet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    int mathematics, english, physics, computer_science, chemistry, maximumMarks = 500;
+    printf("Enter the marks obtained in Mathematics:- ");
+    scanf("%d", &mathematics);
+
+    printf("Enter the marks obtained in English:- ");
+    scanf("%d", &english);
+
+    printf("Enter the marks obtained in Physics:- ");
+    scanf("%d", &physics);
+
+    printf("Enter the marks obtained in Computer Science:- ");
+    scanf("%d", &computer_science);
+
+    printf("Enter the marks obtained in Chemistry:- ");
+    scanf("%d", &chemistry);
+
+    int aggregateMarks = mathematics + physics + computer_science + chemistry + english;
+    
+    float totalPercentage = (mathematics + physics + computer_science + chemistry + english) / maximumMarks * 100;
+    
+    printf("Total marks obtained %d \n", aggregateMarks);
+    printf("Total percentage:- %f", totalPercentage);
+    return 0;
+}
