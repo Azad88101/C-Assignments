501f6447cb05f58aa1429d378b248d73cd362f39
Ansalpandey110043 <pandeyansal77@gmail.com>
2023-04-17 12:34:57 +0530

    Swap Numbers modified

--- Swap_Numbers.c
+++ Swap_Numbers.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int c, d;
+    int c, d; // c = 2 d = 3
 
     printf("Enter the value of c:- ");
     scanf("%d", &c);
@@ -11,9 +11,9 @@ int main() {
     scanf("%d", &d);
 
     printf("Without swapping numbers:- 'c = %d' and 'd = %d' \n" , c, d);
-    c = c + d;
-    d = c - d;
-    c = c - d;
+    c = c + d;  // c = 2 + 3 = 5
+    d = c - d;  // d = 5 - 3 = 2
+    c = c - d;  // c = 5 - 2 = 3
 
     printf("After swapping numbers:- 'c = %d' and 'd = %d'" , c, d);
     return 0;
