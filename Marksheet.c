5c6943cac2d2796198a9bc51edb6ffdf77f3f461
Ansalpandey110043 <pandeyansal77@gmail.com>
2023-04-17 15:52:02 +0530

    Code quality improved

--- Marksheet.c
+++ Marksheet.c
@@ -20,7 +20,7 @@ int main() {
 
     int aggregateMarks = mathematics + physics + computer_science + chemistry + english;
     
-    float totalPercentage = (mathematics + physics + computer_science + chemistry + english) * 100 / maximumMarks; // Error was occured due to Operator Precedence
+    float totalPercentage = aggregateMarks * 100 / maximumMarks; // Error was occured due to Operator Precedence
     
     printf("Total marks obtained %d \n", aggregateMarks);
     printf("Total percentage:- %f", totalPercentage);
